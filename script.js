e99f9762034e404a4e003df27c30b7c0517b46d3
DouglasDG <114673280+DouglasDGA@users.noreply.github.com>
2022-09-30 14:11:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var $h1 = document.getElementById("Subtitulo");
+
+var $div = document.getElementById("Subtitulo");
+
+var $h1 = document.getElementById("Subtitulo").style.color = "#ffff00";
+for(var i = 0; i < 1000; i++);
+for(var i = 0; i < 255; i++)document.getElementById("Subtitulo").style.color = "#ffff00" + i;
+
+console.log($div.firstChild);
+console.log($h1.textContent);
+console.log($h1);
+
+div = document.getElementById("Subtitulo");
+
+//alert(div)
+$div.innerHTML = "Feito por Douglas D.G. de Albuquerque";
+
+
+
+var $h2 = document.getElementById("Mensagem");
+
+var $dir = document.getElementById("Mensagem");
+
+var $h2 = document.getElementById("Mensagem").style.color = "#ff0000";
+for(var i = 0; i < 1000; i++);
+for(var i = 0; i < 255; i++)document.getElementById("Mensagem").style.color = "#ff0000" + i;
+
+console.log($dir.firstChild);
+console.log($h2.textContent);
+console.log($h2);
+
+dir = document.getElementById("Mensagem");
+
+//alert(dir)
+$dir.innerHTML = "Agradecimentos";
